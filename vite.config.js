c93c01ed4ec3d4a59e75e2e748fae1253b54bbe9
Jake-py <jaras3950001@gmail.com>
2025-10-23 17:51:44 +0500

    Обновлён билд для полного сайта. Исправлено пустота сайта после запуска

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Alphearea/',        // путь к репозиторию на GitHub Pages
-  server: { historyApiFallback: true }
+  base: process.env.NODE_ENV === 'production' ? '/Alphearea/' : '/',        // путь к репозиторию на GitHub Pages
+  server: { historyApiFallback: true },
+  build: {
+    sourcemap: false // отключаем eval для карт кода
+  }
 })
