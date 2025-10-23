1ab7bd95105846c40d54fb6eb20b235816310f48
Jake-py <jaras3950001@gmail.com>
2025-10-23 15:42:10 +0500

    Обновлен base для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Alphearea/',        // <--- добавлено для GitHub Pages
   server: { historyApiFallback: true }
 })
