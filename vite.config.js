cb32b8aa2822508b5dc68392cd5e7410b1754e9e
Jake-py <jaras3950001@gmail.com>
2025-10-23 15:52:00 +0500

    Обновлён билд для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Alphearea/',        // <--- добавлено для GitHub Pages
+  base: '/Alphearea/',        // путь к репозиторию на GitHub Pages
   server: { historyApiFallback: true }
 })
